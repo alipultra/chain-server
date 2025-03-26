757790665b5fe18129dea35a50d14faec2a637b6
Alip Putra <beplas.studio@gmail.com>
2025-03-26 13:22:48 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    tun: {
+        host: 'localhost',
+        port: 8089,
+        serverCreds: {
+            user: "xxxxxx",
+            pass: "xxxxxx",
+            authType: "local"
+        },
+        url: 'http://localhost:8089'
+    },
+    rethink: {
+        db: "warehouse"
+    },
+    user: {
+        url: 'http://localhost:7077'
+    }
+}
