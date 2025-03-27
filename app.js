1982132f3011a7addf863ecf8026505ef645c47b
Alip Putra <beplas.studio@gmail.com>
2025-03-27 15:51:00 +0700

    create server handling

--- app.js
+++ app.js
@@ -48,3 +48,28 @@ function onListening() {
         : 'port ' + addr.port;
     console.log('Listening on ' + bind);
 }
+
+// EXIT HANDLING
+
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+    tuner.cleanup().then(() => {
+        console.log('cleanup done');
+        if (err) console.log('err stack:', err.stack);
+        if (options.exit) process.exit();
+    })
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
