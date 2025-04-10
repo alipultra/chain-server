5fae6408e27e2304657e160cbfc452ee52fae54f
Alip Putra <beplas.studio@gmail.com>
2025-04-10 08:30:16 +0700

    add base tun schema

---
+++ tuner/api/base-tun-object.js
@@ -0,0 +1,34 @@
+const moment = require('moment');
+const clone  = require('clone');
+
+var BaseTun = {};
+
+BaseTun.schemaDef = {
+    $schema: 'http://json-schema.org/draft-04/schema#',
+    type: 'object',
+    properties: {
+        history: {
+            type: 'array',
+            items: {
+                type: 'object',
+                properties: {
+                    id: { type: 'string' },
+                    status: { type: 'string' },
+                    date: { type: 'number' }
+                }
+            }
+        },
+        id: { type: 'string' },
+        status: { type: 'string' },
+        date: { type: 'number' }
+    }
+}
+
+BaseTun.createSchema = function(extraProperties) {
+    let schema = clone(BaseTun.schemaDef);
+    Object.assign(schema.properties, clone(extraProperties));
+    Object.assign(schema.properties.history.items.properties, clone(extraProperties));
+    return schema;
+}
+
+module.exports = BaseTun;
